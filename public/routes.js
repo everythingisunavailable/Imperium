26d4a7b90c52b284ce7776ba60197db230daa30b
frenki <frenkiqose@gmail.com>
2025-03-16 18:50:50 +0100

    - builds route system for link redirects without page reload

---
+++ public/routes.js
@@ -0,0 +1,52 @@
+/*
+base url : http://127.0.0.1:5500/public/index.html/product/productId
+what we need : product/productId
+what we going to turn it into : /server/routes.php?query0=product&query1=productId
+excpected to be called on every GET
+*/
+const BASE_URL = "public/";
+function extract_link(){
+    let url = window.location.pathname;
+    let sub_url = url.substring(url.indexOf(BASE_URL) + BASE_URL.length);
+    let sub_url_arr = sub_url.split('/');
+
+    let get_url = '/imperium/server/routes.php?';
+    
+    if (sub_url_arr[0] == '') {
+        return get_url
+    }
+    
+    for (let i = 0; i < sub_url_arr.length; i++) {
+        get_url = get_url + 'query' + i + '=' + sub_url_arr[i];
+        if (sub_url_arr.length - i != 1){ //if we are NOT at the last word
+            get_url = get_url + '&';
+        }
+    }
+
+    return get_url;
+}
+
+
+async function getData() {
+    const url = extract_link();
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+    
+        const data = await response.text();
+
+        //testing
+        document.querySelector('.content').innerHTML = data;
+        //end testing
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+window.onload = async function() {
+    await getData();
+};
--- server/routes.php
+++ server/routes.php
@@ -1,1 +1,25 @@
-// ktu vendoset se ne cfare file theret linku i derguar nga fronti
+<?php
+
+//include database here
+
+
+$query0 = $_GET['query0'] ?? '';
+$query1 = $_GET['query1'] ?? '';
+//add more vars if needed
+
+if ($query0 == 'product' && !$query1) {
+    //TODO : call control functions here
+    
+    echo 'all products to be displayed here'; //TODO : remove this
+}
+else if ($query0 == 'product' && $query1 == 'productId'){
+    //NOTE : productId can be any product Id, probably a
+    //TODO : call control functions here
+    
+    echo 'one specific product to be displayed'; //TODO : remove this
+}
+else {
+    echo 'url is invalid';
+}
+
+?>
