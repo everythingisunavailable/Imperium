5e38943ce0b89a0b9f8974bdf6a2ff691e459060
frenki <frenkiqose@gmail.com>
2025-04-28 18:38:17 +0200

    - complete hero section - add responsivness

--- public/main.js
+++ public/main.js
@@ -110,6 +110,15 @@ if (window.innerWidth > 975) {
 function toggle_ham_menu(){
     document.querySelector('.hamburger').classList.toggle('hide-nav');
 }
+function close_ham_menu(event){
+    if (!document.querySelector('.hamburger').contains(event.target) && !document.querySelector('.ham-button').contains(event.target)) {
+        document.querySelector('.hamburger').classList.add('hide-nav');
+    }
+}
+window.addEventListener('click',  (event)=>{    
+    close_ham_menu(event);
+});
+
 
 // Hero section pc carousel
 function scale_and_move(element, delta_x, delta_y, scale){
@@ -118,21 +127,30 @@ function scale_and_move(element, delta_x, delta_y, scale){
 function reset_element(element){
     element.style.transform = `translate(0,0)`;
 }
+function btn_pop_into_view(btn, index){
+    btn.disabled = !btn.disabled;
+    
+    pop_into_view(index);
+}
 function pop_into_view(index){
     let pc_hero = document.getElementById('hero-right');
     let target = {x: pc_hero.getBoundingClientRect().x + (pc_hero.clientWidth/3), y: pc_hero.getBoundingClientRect().y + pc_hero.clientHeight/2.5};
     
     let pc_array = document.getElementsByClassName('carousel-item');
+    //since the buttons are the parents of the above array elements, then we can traverse them using that array
+    let btn_array = document.getElementsByClassName('pc_btn');
     for (let i = 0; i < pc_array.length; i++) {
         let start_position = {x: pc_array[i].getBoundingClientRect().x ,y: pc_array[i].getBoundingClientRect().y};
         let delta_x = target.x - start_position.x;
         let delta_y = target.y - start_position.y;
 
         if (index == i) {
-            scale_and_move(pc_array[i], delta_x, delta_y, 3.5);
+            scale_and_move(pc_array[i], delta_x, delta_y, 7);
         }
         else{
             reset_element(pc_array[i]);
+            //reset the buttons also
+            btn_array[i].disabled = false;
         }
     }
 }
--- server/view/home.view.php
+++ server/view/home.view.php
@@ -4,31 +4,41 @@ function display_home(){
         <div class="hero">
             <div class="left">
                 <div class="cto">
-                    <h1>Create your Empire or smth</h1>
-                    <p>this is some description of what i said earlier</p>
+                    <h1>Conquer Your Next Build</h1>
+                    <h3>High-performance parts. Unmatched reliability. Built for victory.</h3>
                     <button class="cto-button">BUY NOW</button>
                 </div>
             </div>
 
-            <div class="right" id="hero-right">
+            <div class="right" id="hero-right" >
                 <div class="carousel-wrapper">
                     <div class="carousel">
 
-                        <div class="carousel-item">
-                            <img src="assets/pictures/pc-item.png" alt="prebuild pc" onclick="pop_into_view(0)">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/pictures/pc-item.png" alt="prebuild pc" onclick="pop_into_view(1)">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/pictures/pc-item.png" alt="prebuild pc" onclick="pop_into_view(2)">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/pictures/pc-item.png" alt="prebuild pc" onclick="pop_into_view(3)">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/pictures/pc-item.png" alt="prebuild pc" onclick="pop_into_view(4)">
-                        </div>
+                        <button class="carousel-item-wrapper pc_btn" disabled onclick="btn_pop_into_view(this, 0)">
+                            <div class="carousel-item first-selected">
+                                <img src="assets/pictures/pc-item.png" alt="prebuild pc">
+                            </div>
+                        </button>                    
+                        <button class="carousel-item-wrapper pc_btn" onclick="btn_pop_into_view(this, 1)">
+                            <div class="carousel-item">
+                                <img src="assets/pictures/pc-item.png" alt="prebuild pc">
+                            </div>
+                        </button>                        
+                        <button class="carousel-item-wrapper pc_btn" onclick="btn_pop_into_view(this, 2)">
+                            <div class="carousel-item">
+                                <img src="assets/pictures/pc-item.png" alt="prebuild pc">
+                            </div>
+                        </button>                        
+                        <button class="carousel-item-wrapper pc_btn" onclick="btn_pop_into_view(this, 3)">
+                            <div class="carousel-item">
+                                <img src="assets/pictures/pc-item.png" alt="prebuild pc">
+                            </div>
+                        </button>                        
+                        <button class="carousel-item-wrapper pc_btn" onclick="btn_pop_into_view(this, 4)">
+                            <div class="carousel-item">
+                                <img src="assets/pictures/pc-item.png" alt="prebuild pc">
+                            </div>
+                        </button>                        
 
                     </div>
                 </div>
