ecb0c2b9064da627381bfe6a72b697b33b8f1a67
frenki <frenkiqose@gmail.com>
2025-05-28 00:37:22 +0200

    - add db items to homepage view - add db items to products view - fix filtering

--- server/routes.php
+++ server/routes.php
@@ -5,15 +5,15 @@ $filter = $_GET['filters'] ?? '';
 if (!empty($filter)) $filter = urldecode($filter);
 
 if (!isset($_GET['query0'])) {
-    showHome();
+    showHome(['bestSelling' => 5, 'newProducts' => 5,]);
 } else if ($_GET['query0'] === 'profile' && !isset($_GET['query1'])) {
     showProfile();
 } else if ($_GET['query0'] === 'prebuilts' && !isset($_GET['query1'])) {
-    showProducts();
+    showProducts($filter, 'prebuilt');
 } else if ($_GET['query0'] === 'components' && !isset($_GET['query1'])) {
-    showProducts();
+    showProducts($filter, 'component');
 } else if ($_GET['query0'] === 'peripherals' && !isset($_GET['query1'])) {
-    showProducts();
+    showProducts($filter, 'peripheral');
 } else if ($_GET['query0'] === 'cart' && !isset($_GET['query1'])) {
     showCart();
 } else if ($_GET['query0'] === 'about' && !isset($_GET['query1'])) {
